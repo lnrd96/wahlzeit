59cacec2ef1cdafb3304a8cfd0b0b956c423f052
--replace-all <leonard.fischer@fau.de>
2022-11-07 09:14:19 +0100

    Added class DogPhotoFactory.

---
+++ src/main/java/org/wahlzeit/model/DogPhotoFactory.java
@@ -0,0 +1,65 @@
+package org.wahlzeit.model;
+
+import java.sql.*;
+
+/**
+ * An Abstract Factory for creating dogDogPhotos and related objects.
+ */
+public class DogPhotoFactory extends PhotoFactory {
+    
+	/**
+	 * Hidden singleton instance; needs to be initialized from the outside.
+	 */
+	private static DogPhotoFactory instance = null;
+	
+	/**
+	 * Public singleton access method.
+	 */
+    public static synchronized DogPhotoFactory getInstance() {
+        if (instance == null) {
+            setInstance(new DogPhotoFactory());
+        }
+        return instance;
+    }
+	
+	/**
+	 * Method to set the singleton instance of DogPhotoFactory.
+	 */
+	protected static synchronized void setInstance(DogPhotoFactory dogDogPhotoFactory) {
+		if (instance != null) {
+			throw new IllegalStateException("attempt to initialize DogPhotoFactory twice");
+		}
+		
+		instance = dogDogPhotoFactory;
+	}
+	
+	/**
+	 * 
+	 */
+	protected DogPhotoFactory() {
+		// do nothing
+	}
+
+	/**
+	 * @methodtype factory
+     * @override
+	 */
+	public DogPhoto createPhoto() {
+		return new DogPhoto();
+	}
+    
+	/**
+	 * @override
+	 */
+	public DogPhoto createPhoto(PhotoId id) {
+		return new DogPhoto(id);
+	}
+	
+	/**
+	 * @override
+	 */
+	public DogPhoto createPhoto(ResultSet rs) throws SQLException {
+		return new DogPhoto(rs);
+	}
+
+}
