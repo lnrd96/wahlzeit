9df726d0bfe4a55664aabbff749973f6615ffb11
--replace-all <leonard.fischer@fau.de>
2022-11-13 20:31:59 +0100

    Adapted Location class to work with both coordinate types.

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -34,31 +34,46 @@ public class Location {
 	/**
 	 * 
 	 */
-	public void setCoordinates(double x, double y, double z) {
-		coordinate.setX(x);
-		coordinate.setY(y);
-		coordinate.setZ(z);
+	public void setCartesianCoordinates(double x, double y, double z) {
+		coordinate = new CartesianCoordinate(x, y, z);
 	}
-	public Map<String, Double> getCoordinates() {
+	public void setSphericCoordinates(double phi, double theta, double radius) {
+		coordinate = new SphericCoordinate(phi, theta, radius);
+	}
+	public Map<String, Double> getCartesianCoordinates() {
 		Map<String, Double> coordinates = new HashMap<String, Double>();
-		coordinates.put("x", coordinate.getX());
-		coordinates.put("y", coordinate.getY());
-		coordinates.put("z", coordinate.getZ());
+		coordinates.put("x", coordinate.asCartesianCoordinate().getX());
+		coordinates.put("y", coordinate.asCartesianCoordinate().getY());
+		coordinates.put("z", coordinate.asCartesianCoordinate().getZ());
+		return coordinates;
+	}
+	public Map<String, Double> getSphericCoordinates() {
+		Map<String, Double> coordinates = new HashMap<String, Double>();
+		coordinates.put("phi",    coordinate.asSphericCoordinate().getPhi());
+		coordinates.put("theta",  coordinate.asSphericCoordinate().getTheta());
+		coordinates.put("radius", coordinate.asSphericCoordinate().getRadius());
 		return coordinates;
 	}
 	/**
 	 * 
 	 */
 	public void writeOn(ResultSet rset) throws SQLException {
-		rset.updateDouble("location_coordinate_x", getCoordinates().get("x"));
-		rset.updateDouble("location_coordinate_y", getCoordinates().get("y"));
-		rset.updateDouble("location_coordinate_z", getCoordinates().get("z"));
+		rset.updateDouble("location_coordinate_x", getCartesianCoordinates().get("x"));
+		rset.updateDouble("location_coordinate_y", getCartesianCoordinates().get("y"));
+		rset.updateDouble("location_coordinate_z", getCartesianCoordinates().get("z"));
+		rset.updateDouble("location_coordinate_phi",    getSphericCoordinates().get("phi"));
+		rset.updateDouble("location_coordinate_theta",  getSphericCoordinates().get("theta"));
+		rset.updateDouble("location_coordinate_radius", getSphericCoordinates().get("radius"));
 	}
 	public void readFrom(ResultSet rset) throws SQLException {
 		double x = rset.getDouble("location_coordinate_x");
 		double y = rset.getDouble("location_coordinate_y");
 		double z = rset.getDouble("location_coordinate_z");
-		setCoordinates(x, y, z);
+		setCartesianCoordinates(x, y, z);
+		double phi = rset.getDouble("location_coordinate_phi");
+		double theta = rset.getDouble("location_coordinate_theta");
+		double radius = rset.getDouble("location_coordinate_radius");
+		setSphericCoordinates(phi, theta, radius);
 	}
 
 }
