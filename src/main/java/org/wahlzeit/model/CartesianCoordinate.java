d50082938f876cd2a534639c3f8fcdd273675667
--replace-all <leonard.fischer@fau.de>
2022-11-28 07:22:48 +0100

    Added class-invariants and pre-/post-condition assertions to CartesianCoordinate. Added doGetCartesianDistance() and doAsSphericCoordinate().

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -3,11 +3,6 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Objects;
 
-/**
- * Some Notes:
- * The names of the two classes are used only to create the objects.
- * Coordinate c = CartesianCoordinate();
- */
 
 
 public class CartesianCoordinate extends AbstractCoordinate {
@@ -25,9 +20,8 @@ public class CartesianCoordinate extends AbstractCoordinate {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.assertClassInvariance();
     }
-
-    public CartesianCoordinate() { }
 
     /**
      * Getters and Setters
@@ -58,20 +52,52 @@ public class CartesianCoordinate extends AbstractCoordinate {
 
     @Override
     public CartesianCoordinate asCartesianCoordinate() {
+        this.assertClassInvariance();
         return this;
     }
 
+
     @Override
     public double getCartesianDistance(Coordinate coordinate) {
+        
         CartesianCoordinate other = coordinate.asCartesianCoordinate();
-        double distance = Math.sqrt(Math.pow(this.getX() - other.getX(), 2)
-                                  + Math.pow(this.getY() - other.getY(), 2)
-                                  + Math.pow(this.getZ() - other.getZ(), 2));
+        
+        // class invariant assertion
+        this.assertClassInvariance();
+        other.assertClassInvariance();
+        
+        // no precondition
+        double distance = doGetCartesianDistance(other);
+        assert distance >= 0.0;  // post condition
+
+        // class invariant assertion
+        this.assertClassInvariance();
+        other.assertClassInvariance();
+        
         return distance;
     }
+    
+    protected double doGetCartesianDistance(CartesianCoordinate other) {
+        return  Math.sqrt(Math.pow(this.getX() - other.getX(), 2)
+                        + Math.pow(this.getY() - other.getY(), 2)
+                        + Math.pow(this.getZ() - other.getZ(), 2));
+    }
+
 
     @Override
     public SphericCoordinate asSphericCoordinate() {
+        // class invariant assertion
+        this.assertClassInvariance();
+        
+        SphericCoordinate converted = doAsSphericCoordinate();
+        
+        // class invariant assertion
+        this.assertClassInvariance();
+
+        return converted;
+    }
+    
+    protected SphericCoordinate doAsSphericCoordinate() {
         double radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
         double theta;
         if (x != 0.0) {
@@ -85,20 +111,49 @@ public class CartesianCoordinate extends AbstractCoordinate {
 
     @Override
     public double getCentralAngle(Coordinate coordinate) {
+        // class invariant assertion
+        this.assertClassInvariance();
         SphericCoordinate other = coordinate.asSphericCoordinate();
-        return other.getCentralAngle(this);
-    }
 
+        // no pre conditions
+        double centralAngle = other.getCentralAngle(this);  // delegate to other class
+        assert centralAngle >= 0.0;  // post condition
+
+        // class invariant assertion
+        this.assertClassInvariance();
+        
+        return centralAngle;
+    }
+    
     @Override
     public int hashCode() {
+       
+        // class invariant assertion
+        this.assertClassInvariance();
+        
         String pattern = "0.0000";  // somehow derive it from TOLERANCE?
         DecimalFormat df = new DecimalFormat(pattern);
         df.setRoundingMode(RoundingMode.CEILING);
         String rounded_x = df.format(x);
         String rounded_y = df.format(y);
         String rounded_z = df.format(z);
-        return Objects.hash(rounded_x, rounded_y, rounded_z);
+        int hashValue = Objects.hash(rounded_x, rounded_y, rounded_z);
+        
+        // class invariant assertion
+        this.assertClassInvariance();
+
+        return hashValue;
     }
 
+    /**
+     * class invariant assertion functions
+     */
+    @Override
+    protected void assertClassInvariance() {
+        if (Math.abs(Math.sqrt(Math.sqrt(x) + Math.sqrt(y) + Math.sqrt(z)) - WORLD_RADIUS_KM) < TOLERANCE * 100) {
+            System.out.print("Only dog photos taken on planet earth are allowed for animal right reasons.");
+            // throw dedicated exception from next homework on.
+        }
+    }
 
 }
