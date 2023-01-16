57090c7955ec74b195fab202f3a673b2f89f365a
--replace-all <leonard.fischer@fau.de>
2023-01-16 09:18:42 +0100

    Added type hierarchy feature to the type class.

--- src/main/java/org/wahlzeit/model/DogType.java
+++ src/main/java/org/wahlzeit/model/DogType.java
@@ -3,6 +3,8 @@ package org.wahlzeit.model;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.wahlzeit.services.DataObject;
 import org.wahlzeit.utils.PatternInstance;
@@ -17,6 +19,8 @@ public class DogType extends DataObject {
      * Type class providing type specific functionality.
      */
     protected String typeName;
+    private DogType supertype = null;
+    private Set<DogType> subtypes = new HashSet<DogType>();
 
     public DogType(String typeName) {
         this.typeName = typeName;
@@ -33,6 +37,47 @@ public class DogType extends DataObject {
         return new Dog(name, age, this);
     }
 
+    /**
+     * Functions to handle and inspect the type hierarchie
+     */
+    // Getter for supertype
+    public DogType getSupertype() {
+        return supertype;
+    }
+
+    // Setter for supertype
+    public void setSupertype(DogType supertype) {
+        this.supertype = supertype;
+    }
+
+    // Getter for subtypes
+    public Set<DogType> getSubtypes() {
+        return subtypes;
+    }
+
+    // Setter for subtypes
+    public void setSubtypes(Set<DogType> subtypes) {
+        this.subtypes = subtypes;
+    }
+
+    // Add a subtype
+    public void addSubtype(DogType subtype) {
+        subtypes.add(subtype);
+    }
+
+    // Remove a subtype
+    public void removeSubtype(DogType subtype) {
+        subtypes.remove(subtype);
+    }
+
+    // Check if the type has a subtype
+    public boolean hasSubtype(DogType subtype) {
+        return subtypes.contains(subtype);
+    }
+
+    /**
+     * Type specific methods
+     */
     public int getAverageLifespan() {
         // this is just an example for type-specific functionality
         return 11;
